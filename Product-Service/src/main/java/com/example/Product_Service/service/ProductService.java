256b387c2356938e81f28088e653cd184a3f422c
Samaan Kamal <samaankamal72@gmail.com>
2025-03-09 23:05:31 +0200

    added product service with create product method

---
+++ Product-Service/src/main/java/com/example/Product_Service/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.example.Product_Service.service;
+
+import com.example.Product_Service.dto.ProductRequest;
+import com.example.Product_Service.model.Product;
+import com.example.Product_Service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder().name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+}
