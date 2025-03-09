c5380e81b5770facf0b53f9516693712ead9ea7b
Samaan Kamal <samaankamal72@gmail.com>
2025-03-09 23:04:59 +0200

    added product repository

---
+++ Product-Service/src/main/java/com/example/Product_Service/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.Product_Service.repository;
+
+import com.example.Product_Service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
