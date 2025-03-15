33eb7f057e7952753bd9440ce1b0cf1ef7c2ec55
Samaan Kamal <samaankamal72@gmail.com>
2025-03-15 22:44:56 +0200

    added property source for environment property

--- Order-Service/src/main/java/com/example/Order_Service/OrderServiceApplication.java
+++ Order-Service/src/main/java/com/example/Order_Service/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.Order_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
