211e1acbf6b87375ba9da6cdf758c13f4e6e47d3
Samaan Kamal <samaankamal72@gmail.com>
2025-03-21 23:32:20 +0200

    added inventory entity

---
+++ Inventory-Service/src/main/java/com/example/Inventory_Service/model/Inventory.java
@@ -0,0 +1,15 @@
+package com.example.Inventory_Service.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "t_inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
