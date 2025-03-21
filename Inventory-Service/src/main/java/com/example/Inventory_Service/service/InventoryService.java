7b2bd42d4ce171ce684335f142aed5924368e479
Samaan Kamal <samaankamal72@gmail.com>
2025-03-21 23:34:32 +0200

    added inventory service with business logic to check the item availability

---
+++ Inventory-Service/src/main/java/com/example/Inventory_Service/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.example.Inventory_Service.service;
+
+import com.example.Inventory_Service.model.Inventory;
+import com.example.Inventory_Service.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode){
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+}
