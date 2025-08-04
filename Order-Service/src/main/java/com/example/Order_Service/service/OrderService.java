33555c9ae2080309eb46a29ab0c7c42c42bbfb68
Samaan Khella (EXT-Nokia) <samaan.khella.ext@nokia.com>
2025-08-04 11:11:18 +0300

    added request call to check for products availability

--- Order-Service/src/main/java/com/example/Order_Service/service/OrderService.java
+++ Order-Service/src/main/java/com/example/Order_Service/service/OrderService.java
@@ -8,6 +8,7 @@ import com.example.Order_Service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,13 +19,20 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList().stream().map(this::mapToEntity).toList();
         order.setOrderLineItemsList(orderLineItemsList);
-        orderRepository.save(order);
+        boolean result = webClient.get().uri("http://localhost:8082/api/inventory/").retrieve().bodyToMono(Boolean.class).block();
+        if(result){
+            orderRepository.save(order);
+        }
+        else{
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderLineItems mapToEntity(OrderLineItemsDto orderLineItemsDto){
