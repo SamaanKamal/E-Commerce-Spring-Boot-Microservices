08e1f3c1c91b5e37dcbaed3bfa45738dd407788b
Samaan Khella (EXT-Nokia) <samaan.khella.ext@nokia.com>
2025-08-04 11:44:36 +0300

    modified return type and body of the is in stock method

--- Inventory-Service/src/main/java/com/example/Inventory_Service/service/InventoryService.java
+++ Inventory-Service/src/main/java/com/example/Inventory_Service/service/InventoryService.java
@@ -1,10 +1,14 @@
 package com.example.Inventory_Service.service;
 
+import com.example.Inventory_Service.dto.InventoryResponse;
 import com.example.Inventory_Service.model.Inventory;
 import com.example.Inventory_Service.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +16,7 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream().map(inventory ->  InventoryResponse.builder().skuCode(inventory.getSkuCode()).isInStock(inventory.getQuantity() > 0).build()).toList();
     }
 }
