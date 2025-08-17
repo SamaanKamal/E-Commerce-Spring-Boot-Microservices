185d1382b89daeaa549a464d7c2eae1d9d917653
Samaan Khella (EXT-Nokia) <samaan.khella.ext@nokia.com>
2025-08-17 16:29:15 +0300

    added logs with thread sleep for 10 seconds

--- Inventory-Service/src/main/java/com/example/Inventory_Service/service/InventoryService.java
+++ Inventory-Service/src/main/java/com/example/Inventory_Service/service/InventoryService.java
@@ -4,6 +4,8 @@ import com.example.Inventory_Service.dto.InventoryResponse;
 import com.example.Inventory_Service.model.Inventory;
 import com.example.Inventory_Service.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,11 +14,16 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode){
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream().map(inventory ->  InventoryResponse.builder().skuCode(inventory.getSkuCode()).isInStock(inventory.getQuantity() > 0).build()).toList();
     }
 }
