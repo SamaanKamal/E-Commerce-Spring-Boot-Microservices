6c648c656e7cb96c85f6e6ba22205b6173fa2444
Samaan Khella (EXT-Nokia) <samaan.khella.ext@nokia.com>
2025-08-17 14:51:08 +0300

    added property source annotation

--- Discovery-Server/src/main/java/com/example/Discovery_Server/DiscoveryServerApplication.java
+++ Discovery-Server/src/main/java/com/example/Discovery_Server/DiscoveryServerApplication.java
@@ -3,9 +3,11 @@ package com.example.Discovery_Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableEurekaServer
+@PropertySource("classpath:env.properties")
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
